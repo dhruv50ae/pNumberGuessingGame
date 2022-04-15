be3c561139632a451ac9035012d007109981ef4b
dhruv50ae <linkdp2012@gmail.com>
2022-04-15 14:18:40 +0100

    Endgame commit

--- main.py
+++ main.py
@@ -1,10 +1,50 @@
-enemies = 1
 
 
-def increaseEnemies():
-    enemies = 2
-    print(f"Enemies inside function: {enemies}")
+import random
+
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+turns = 0
 
 
-increaseEnemies()
-print(f"Enemies outside function: {enemies}")
+def checkAnswer(guess, answer, turns):
+    if guess > answer:
+        print("Too high")
+        return turns - 1
+    elif guess < answer:
+        print("Too low")
+        return turns - 1
+    else:
+        print("You got the answer")
+
+
+def setDifficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if level == 'easy':
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
+
+
+def game():
+
+    print("Welcome to the Number Guessing Game")
+    print("I'm thinking of a number between 1 and 100")
+
+    answer = random.randint(1, 100)
+
+    turns = setDifficulty()
+
+    guess = 0
+    while guess != answer:
+        print(f"You have {turns} attempts remaining to guess the number.")
+
+        guess = int(input("Make a guess: "))
+
+        turns = checkAnswer(guess, answer, turns)
+        if turns == 0:
+            print("You ran out of turns")
+            return
+
+
+game()
